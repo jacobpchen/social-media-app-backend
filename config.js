cf053a91a1f9b43817da9eccceb3b4632220e8f3
Jacob Chen <jacobpchen@gmail.com>
2021-01-22 09:54:16 -0500

    upload finished project

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://blankhank:9OmL0hqu1DpqZcTf@cluster0.xfobv.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'this is a super secure password12345'
+}
